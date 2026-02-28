9d92ddd737e5979564702a279a0a2f55f521c7f2
Jade <spacey-sooty@proton.me>
2026-02-28 20:59:19 +0800

    chore: enable Phoenix and DataLogManager loggers (#84)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -2,11 +2,13 @@ package org.curtinfrc.frc2026;
 
 import static org.curtinfrc.frc2026.vision.Vision.cameraConfigs;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -110,6 +112,9 @@ public class Robot extends LoggedRobot {
     }
 
     Logger.start();
+    SignalLogger.start();
+    DataLogManager.start();
+
     if (Constants.getMode() != Constants.Mode.REPLAY) {
       switch (Constants.robotType) {
         case COMP -> {
